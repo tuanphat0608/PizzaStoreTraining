a7f028b8ba6c398ece413115568bcc72ab300d66
Phat Duong <tuanphat0608@gmail.com>
2025-04-09 09:30:22 +0700

    update register function

--- src/main/java/com/pizza/service/implement/UserServiceImpl.java
+++ src/main/java/com/pizza/service/implement/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.pizza.dto.UserDTO;
 import com.pizza.enums.Role;
 import com.pizza.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.pizza.repository.UserRepository;
 import com.pizza.service.UserService;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -54,6 +56,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public void registerUser(UserDTO registerRequest) {
         User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        Optional<User> userOpt = userRepository.findByUsername(registerRequest.getUsername());
+        if (userOpt.isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "User already exist");
+        }
         user.setUsername(registerRequest.getUsername());
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword())); // Encode password
         user.setRole(Role.valueOf(registerRequest.getRole()));
